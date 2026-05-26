bb51e861d67e1f1a6363812d0565ea59110b64ec
icder <push@icder.com>
2026-05-26 18:59:45 +0300

    remove temp bat file

--- _git_push.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-git config user.email "push@icder.com"
-git config user.name "icder"
-git add .
-git commit -m "ilk commit: icder kurban video sitesi"
-git branch -M main
-git push -u origin main
-echo DONE
