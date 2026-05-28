ce993590a439b3f880d86b017e5296ee11d6632c
icder <push@icder.com>
2026-05-28 18:25:06 +0300

    video degistir ozelligi, etiket arama duzeltme, sidebar temizleme

--- push.bat
+++ push.bat
@@ -1,7 +1,7 @@
 @echo off
 cd /d C:\Users\OrtaPC\Documents\icderrr-main\kurban-video
 git add .
-git commit -m "feat: icder otomatik bagisci aktarimi, org listesi, duplicate kontrol"
+git commit -m "video degistir ozelligi, etiket arama duzeltme, sidebar temizleme"
 git push
 echo PUSH_TAMAM
 pause
